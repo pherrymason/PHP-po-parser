cf7883102e0a10f4cdc565d04d437401208d04ec
Raúl Ferràs <raul@viscaweb.com>
2018-02-01 20:56:54 +0100

    Implement Catalog and Entry entities

---
+++ src/Catalog.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Sepia\PoParser;
+
+use Sepia\PoParser\Catalog\Entry;
+
+class Catalog
+{
+    /** @var array */
+    protected $headers;
+
+    /** @var array */
+    protected $entries;
+
+    /**
+     * @param Entry[] $entries
+     */
+    public function __construct(array $entries = array())
+    {
+        foreach ($entries as $entry) {
+            $this->addEntry($entry);
+        }
+    }
+
+    public function addEntry(Entry $entry)
+    {
+        $key = $this->getEntryHash(
+            $entry->getMsgId(),
+            $entry->getMsgCtxt()
+        );
+        $this->entries[$key] = $entry;
+    }
+
+    public function addHeaders(array $headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @return Entry[]
+     */
+    public function getEntries()
+    {
+        return $this->entries;
+    }
+
+    /**
+     * @param string      $msgId
+     * @param string|null $context
+     *
+     * @return Entry|null
+     */
+    public function getEntry($msgId, $context = null)
+    {
+        $key = $this->getEntryHash($msgId, $context);
+        if (!isset($this->entries[$key])) {
+            return null;
+        }
+
+        return $this->entries[$key];
+    }
+
+    /**
+     * @param string      $msgId
+     * @param string|null $context
+     *
+     * @return string
+     */
+    private function getEntryHash($msgId, $context = null)
+    {
+        return md5($msgId.$context);
+    }
+}
---
+++ src/Catalog/EntryFactory.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Sepia\PoParser\Catalog;
+
+class EntryFactory
+{
+    /**
+     * @param array $entry
+     * @return Entry
+     */
+    public static function createFromArray(array $entryArray)
+    {
+        $entry = new Entry(
+            $entryArray['msgid'],
+            isset($entryArray['msgstr']) ? $entryArray['msgstr'] : null
+        );
+        $plurals = array();
+
+        foreach ($entryArray as $key => $value) {
+            switch (true) {
+                case $key === 'msgctxt':
+                    $entry->setMsgCtxt($entryArray['msgctxt']);
+                    break;
+
+                case $key === 'flags':
+                    $entry->setFlags($entryArray['flags']);
+                    break;
+
+                case $key === 'reference':
+                    $entry->setReference($entryArray['reference']);
+                    break;
+
+                case $key === 'previous':
+                    $entry->setPreviousEntry(self::createFromArray($entryArray['previous']));
+                    break;
+
+                case $key === 'tcomment':
+                    $entry->setTranslatorComments($value);
+                    break;
+
+                case $key === 'ccomment':
+                    $entry->setDeveloperComments($value);
+                    break;
+
+                case $key === 'obsolete':
+                    $entry->setObsolete(true);
+                    break;
+
+                case 0 === strpos($key, 'msgstr['):
+                    $plurals[] = $value;
+                    break;
+            }
+        }
+
+        if (count($plurals) > 0) {
+            $entry->setMsgStrPlurals($plurals);
+        }
+
+        return $entry;
+    }
+}
